4a82164ebee44257f8799133e8c7982d3c5a66f6
Danilo Carvalho Calado <dcalado.fisio@yahoo.com.br>
2023-06-22 14:31:35 -0300

    att

--- main.py
+++ main.py
@@ -9,50 +9,68 @@ pdf_path = "Relatório de Diagnóstico Mind The Bizz 2022.1.pdf"
 text = extract_text(pdf_path)
 
 # Divide o texto em páginas
-pages = text.split('\x0c')[1:]  # Ignora a primeira página
+pages = text.split('\x0c')  # Separa todas as páginas
 
 # Cria um novo arquivo Excel
 wb = Workbook()
 ws = wb.active
 
-# Adicionar rótulos das colunas (somente na primeira página)
+# Adicionar labels das colunas
 col_labels = [
     "Nome do projeto/negócio mentorado",
     "Setor/Segmento de atuação",
-    "Responsável 1 | Profissão | Telefone | E-mail",
-    "Responsável 2 | Profissão | Telefone | E-mail",
+    "Responsável 1 | Profissão | E-mail | Telefone",
+    "Responsável 2 | Profissão | E-mail | Telefone",
     "Local de Origem",
     "Nível de maturidade do projeto",
     "Resumo Diagnóstico",
     "Status de Desenvolvimento de Entregas",
     "Nome do mentor responsável pelo projeto/negócio"
 ]
-
+# Configurar estilo das células dos labels das colunas
 for col_num, label in enumerate(col_labels, start=1):
-    cell = ws.cell(row=1, column=col_num, value=label)
+    cell = ws.cell(row=1, column=col_num, value=label.strip())  # Remover espaços em branco
     cell.alignment = Alignment(horizontal='center', vertical='center')
     cell.fill = PatternFill(start_color="D3D3D3", end_color="D3D3D3", fill_type="solid")  # Cor cinza
-
-# Percorre as páginas e preenche as células correspondentes
-row_num = 2  # Inicia na segunda linha (após os rótulos das colunas)
-
-for page in pages:
-    # Separa o texto da página em linhas
-    lines = page.split('\n')
+    ws.column_dimensions[ws.cell(row=1, column=col_num).column_letter].width = len(label.strip()) + 5
     
-    # Remove as três primeiras linhas da página
-    lines = lines[3:]
+# Percorrer as páginas e preencher as células correspondentes
+row_num = 2  # Inicia na segunda linha (após os labels das colunas)
+for page_num, page in enumerate(pages, start=1):
     
-    for line in lines:
-        line = line.strip()  # Remove espaços em branco no início e no fim da linha
+    # Verificar se é a primeira página para ignorar completamente
+    if page_num == 1:
+        continue
+    
+    # Verificar se é uma página subsequente para remover as primeiras frases
+    if page_num >= 2:
         
-        if line and line not in col_labels:  # Verifica se a linha não está vazia e não é uma label repetida
-            cell = ws.cell(row=row_num, column=col_num, value=line)
-            col_num += 1  # Avança para a próxima coluna
-            
-            if col_num > len(col_labels):  # Verifica se chegou à última coluna
-                row_num += 1  # Avança para a próxima linha
-                col_num = 1  # Reinicia a contagem das colunas
-
+        lines = page.split('\n')[8:]  # Remove as primeiras linhas
+    else:
+        lines = page.split('\n')
+    phrases = [
+        "Nome do Projeto",
+        "Segmento de atuação",
+        "Responsável 1 | Profissão | E-mail | Telefone",
+        "Responsável 2 | Profissão | E-mail | Telefone",
+        "Local de origem",
+        "Nível de maturidade do projeto",
+        "Ideação",
+        "Ideação + Protótipo",
+        "Relatório Intermediário de Acompanhamento | Resumo",
+        "Relatório de Diagnóstico | Resumo",
+        "Status de Desenvolvimento de Entregas | Mind The Bizz",
+        "Mentor(a):"
+    ]
+    
+    # Remover as palavras indesejadas que estão nas frases
+    lines = [line for line in lines if not any(word in line for word in phrases)]
+    
+    # Preencher as células da linha com as informações da página
+    for col_num, line in enumerate(lines, start=1):
+        cell = ws.cell(row=row_num, column=col_num, value=line.strip())  # Remover espaços em branco
+        cell.alignment = Alignment(horizontal='center', vertical='center')
+    row_num += 1
+    
 # Salvar o arquivo Excel
 wb.save("teste.xlsx")
